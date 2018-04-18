f73f743d4cdb8b8528f83d16dc890a04f4fff4d6
Vladimir <punkmusic@ukr.net>
2018-04-18 18:57:25 +0200

    Commit webdevblog-one
    webdevblog-one

---
+++ webdevblog-one/js/costum.js
@@ -0,0 +1,14 @@
+
+$(function() {
+    var link = $('.m-menu-link');
+    var close = $('.close-menu');
+    var menu = $('.m-menu');
+    link.on('click', function(event) {
+        event.preventDefault();
+        menu.toggleClass('m-menu__active')
+    });
+    close.on('click', function(event) {
+        event.preventDefault();
+        menu.toggleClass('m-menu__active')
+    });
+});
